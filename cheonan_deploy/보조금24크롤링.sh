9065e0f8bf12359a9d88e52748ff6b5abbac9cd3
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 <ngoodsamari@naver.com>
2023-02-05 15:39:12 +0900

    add deploy folder

---
+++ cheonan_deploy/보조금24크롤링.sh
@@ -0,0 +1,1 @@
+docker run -it -v $(pwd)/data_before:/cheonan/data_before -v $(pwd)/data_after:/cheonan/data_after -v $(pwd)/do_not_remove:/cheonan/do_not_remove --rm dgoodsamari/cheonan:0.1 python main.py --type "보조금24크롤링"
